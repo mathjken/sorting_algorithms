1984066816abfd34ff89daeef5fa2850988844af
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-10-11 22:44:06 +0100

    ad prototypes

--- sort.h
+++ sort.h
@@ -1,7 +1,8 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef SORT_H_
+#define SORT_H_
 
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -22,5 +23,20 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t **node1, listint_t **node2);
+void selection_sort(int *array, size_t size);
+void swap_integers(int *a, int *b);
+void quick_sort(int *array, size_t size);
+listint_t *create_listint(const int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
