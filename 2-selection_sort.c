9e792646b8dd12250044956dbe984207e4e224f4
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-10-11 23:36:20 +0100

    a function that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,42 +2,39 @@
 
 /**
  * swap - swaps two numbers
- * @num1: first number
- * @num2: second number
+ * @n1: first number
+ * @n2: second number
  */
-void swap(int *num1, int *num2)
+void swap(int *n1, int *n2)
 {
 	int tmp;
 
-	tmp = *num1;
-	*num1 = *num2;
-	*num2 =  tmp;
+	tmp = *n1;
+	*n1 = *n2;
+	*n2 =  tmp;
 }
 
 /**
- * selection_sort - sorts an array in ascending order
- * by selects the smallest element from an unsorted list
- * in each iteration and places that element at the beginning
- * of the unsorted list
+ * selection_sort - sorts an array in ascending orde
  *
  * @array: array of integer
  * @size: number of elements in @array
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
+	size_t x, z, small;
 
-	for (i = 0; i < size - 1; i++)
+	for (x = 0; x < size - 1; x++)
 	{
-		min = i;
-		for (j = i + 1; j < size; j++)
+		small = x;
+		for (z = x + 1; z < size; z++)
 		{
-			if (array[j] < array[min])
-				min = j;
+			if (array[z] < array[small])
+				small = z;
 		}
-		if (min != i)
+		if (small != x)
 		{
-			swap(&array[min], &array[i]);
+			swap(&array[small], &array[x]);
 			print_array(array, size);
 		}
 	}
