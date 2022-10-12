5f3373b24396bb749870cd79bf42bf163d73dfb5
Andrew Eshak Wreford <46035160+wrefinity@users.noreply.github.com>
2022-10-12 01:07:29 +0100

    a function that sorts a deck of cards.

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,29 +1,69 @@
 #include "deck.h"
 
+
+/**
+ * _strcmp - compares two strings
+ * @str1: first string to compare
+ * @str2: second string to compare
+ *
+ * Return: 0 if else 1
+ */
+int _strcmp(const char *str1, const char *str2)
+{
+	while (*str1 == *str2)
+	{
+		if (*str1 == '\0')
+		{
+			return (0);
+		}
+		str1++;
+		str2++;
+	}
+	return (*str1 - *str2);
+}
+
+/**
+ * deck_len - returns length of list
+ * @list: head of list
+ *
+ * Return: length
+ */
+size_t deck_len(deck_node_t *list)
+{
+	size_t x = 0;
+
+	while (list)
+	{
+		x++;
+		list = list->next;
+	}
+	return (x);
+}
+
 /**
  * sort_deck - sorts a deck of card
  * @deck: doubly linked list to sort
  */
 void sort_deck(deck_node_t **deck)
 {
-	deck_node_t *curr;
+	deck_node_t *node;
 	size_t len;
 	deck_node_t *one, *two, *three, *four;
 
-	len = list_len_deck(*deck);
+	len = deck_len(*deck);
 
 	if (!deck || !*deck || len < 2)
 		return;
 
-	curr = *deck;
-	while (curr)
+	node = *deck;
+	while (node)
 	{
-		if (curr->prev && card_value(curr) < card_value(curr->prev))
+		if (node->prev && get_card_value(node) < get_card_value(node->prev))
 		{
-			one = curr->prev->prev;
-			two = curr->prev;
-			three = curr;
-			four = curr->next;
+			one = node->prev->prev;
+			two = node->prev;
+			three = node;
+			four = node->next;
 
 			two->next = four;
 			if (four)
@@ -35,78 +75,38 @@ void sort_deck(deck_node_t **deck)
 			else
 				*deck = three;
 			two->prev = three;
-			curr = *deck;
+			node = *deck;
 			continue;
 		}
 		else
-			curr = curr->next;
+			node = node->next;
 	}
 }
 
 /**
- * card_value - returns the value of a card
+ * get_card_value - returns the value of a card
  * @node: card in a deck
  *
  * Return: value between 1 and 52
  */
-int card_value(deck_node_t *node)
+int get_card_value(deck_node_t *node)
 {
 	char *val[13] = {"Ace", "2", "3", "4", "5", "6",
 		"7", "8", "9", "10", "Jack", "Queen", "King"};
-	char *kinds[4] = {"SPADE", "HEART", "CLUB", "DIAMOND"};
-	int i, kind_val = 0;
+	char *cards[4] = {"SPADE", "HEART", "CLUB", "DIAMOND"};
+	int i, card_val = 0;
 
 	for (i = 1; i <= 13; i++)
 	{
 		if (!_strcmp(node->card->value, val[i - 1]))
-			kind_val = i;
+			card_val = i;
 	}
 
 	for (i = 1; i <= 4; i++)
 	{
-		if (!_strcmp(kinds[node->card->kind], kinds[i - 1]))
-			kind_val = kind_val + (13 * i);
+		if (!_strcmp(cards[node->card->kind], cards[i - 1]))
+			card_val = card_val + (13 * i);
 	}
 
-	return (kind_val);
-}
-
-/**
- * _strcmp - compares two strings
- * @s1: first string to compare
- * @s2: second string to compare
- *
- * Return: less than 0 if s1 is less than s2, 0 if they're equal,
- * more than 0 if s1 is greater than s2
- */
-int _strcmp(const char *s1, const char *s2)
-{
-	while (*s1 == *s2)
-	{
-		if (*s1 == '\0')
-		{
-			return (0);
-		}
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
-}
-
-/**
- * list_len_deck - function returns length of list
- * @list: head of list
- *
- * Return: length
- */
-size_t list_len_deck(deck_node_t *list)
-{
-	size_t len = 0;
-
-	while (list)
-	{
-		len++;
-		list = list->next;
-	}
-	return (len);
+	return (card_val);
 }
